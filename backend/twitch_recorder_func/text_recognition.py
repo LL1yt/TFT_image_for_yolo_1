0ad141993add0d63378a47eb3549221f794f15fc
n0name <pyst0e@me.com>
2024-05-24 13:40:15 +0300

    small fixes

--- backend/twitch_recorder_func/text_recognition.py
+++ backend/twitch_recorder_func/text_recognition.py
@@ -142,7 +142,7 @@ class VideoTextRecognition:
             for f in os.listdir(directory_path)
             if os.path.isfile(os.path.join(directory_path, f))
         )
-    @staticmethod
+    
     def image_label_recod(self, frame, champion_coordinates):
         """Record image label for a given file in the directory."""
         image_id = str(uuid.uuid1())
@@ -195,7 +195,7 @@ class VideoTextRecognition:
             
           ):
             logging.info(
-                f"file_count < self.number_imgs and ChampionChecker.is_champion_missing"
+                f"5) file_count < self.number_imgs and ChampionChecker.is_champion_missing"
             )
             self.image_label_recod(frame, champion_coordinates)
 
@@ -206,7 +206,7 @@ class VideoTextRecognition:
             and (len(self.detected_champion_names) <= len(self.champion_names))
           ):
             logging.info(
-                f"(len(self.detected_champion_names) < len(self.champion_names) and ChampionChecker.is_champion_missing"
+                f"5) (len(self.detected_champion_names) < len(self.champion_names) and ChampionChecker.is_champion_missing"
             )
             self.image_label_recod(frame, champion_coordinates)
 
@@ -214,18 +214,19 @@ class VideoTextRecognition:
             and (not self.Detected_Classes_Class.all_classes_mentioned_three_times(self.count_class_mentions))
           ):
             logging.info(
-                f"(check_classes_with_few_mentions and not all_classes_mentioned_three_times"
+                f"5) (check_classes_with_few_mentions and not all_classes_mentioned_three_times"
             )
             self.image_label_recod(frame, champion_coordinates)
 
         elif(self.Detected_Classes_Class.all_classes_mentioned_three_times(self.count_class_mentions)):
             ImageDatasetSplitter(self.IMAGES_PATH).split_dataset()
+            ImageDatasetSplitter(self.LABELIMG_PATH).split_dataset()
             logging.info(
-                f"all_classes_mentioned_three_times"
+                f"5) all_classes_mentioned_three_times"
             )
             exit()
             
         else:
             logging.info(
-                f"else process_champion_coordinates self.count_class_mentions: {self.count_class_mentions}"
+                f"5) else process_champion_coordinates self.count_class_mentions: {self.count_class_mentions}"
             )
